7158d8f67aa771990c20dfc81e19c558cb73fb95
jeremi <jeremi@072c38d6-2bfb-0310-b348-8b94f9e9609f>
2005-07-23 08:41:10 +0000

    refactore js

---
+++ trunk/server/src/main/web/document.js
@@ -0,0 +1,104 @@
+function getUpdates()
+{
+   try {
+        if (getDocumentName())
+        {
+            var url = baseUrl + "getupdates/" + getWorkspaceName() + "/" + getDocumentName() + "?sinceversion=" + getVersion();
+            executeCommand(url, getUpdatesCallback);
+            if (editing)
+                updateBlock(isError);
+        }
+   }
+    catch (e)
+    {
+        addMessage(e);
+    }
+    setTimeout("getUpdates()", 2000);
+}
+
+function getUpdatesCallback(xml){
+    if (isError(xml))
+        return;
+    readBlocks(xml);
+    setVersion(xml.getElementsByTagName('blocks')[0].getAttribute('version'));
+}
+
+
+function addBlock(blockId)
+{
+    if (editing)
+    {
+        addMessage("You can edit only one block at a time");
+        return;
+    }
+    var previousBlock = document.getElementById(blockId);
+    var pos = 0;
+    if (previousBlock != null){
+        var nodes = previousBlock.childNodes;
+        for (j=0;j<nodes.length;j++)
+        {
+            if (nodes[j].className == "position")
+            {
+                pos = Math.round(nodes[j].innerHTML) + 1;
+                break;
+            }
+        }
+    }
+    var url = baseUrl + "addblock/" + getWorkspaceName() + "/" + getDocumentName() + "?position=" + pos;
+    executeCommand(url, addBlockCallback);
+}
+
+function addBlockCallback(xml){
+    if (isError(xml))
+        return;
+    var blockEl = readBlock(xml);
+    var nodes = blockEl.childNodes;
+    for (var j=0;j<nodes.length;j++)
+    {
+        if (nodes[j].className == "content")
+        {
+            editBlock(nodes[j]);
+            break;
+        }
+    }
+
+}
+
+function getDocument(workspace, document)
+{
+    var url = baseUrl + "getdocument/" + workspace + "/" + document;
+    affDocumentInfos();
+    executeCommand(url, readDocument);
+}
+
+function createDocument(workspace, document)
+{
+    var url = baseUrl + "createdocument/" + workspace + "/" + document;
+    affDocumentInfos();
+    executeCommand(url, readDocument);
+}
+
+
+function readDocument(xml)
+{
+    if (isError(xml))
+    {
+        affOpenFunctions();
+        return;
+    }
+    setVersion(xml.getElementsByTagName('version')[0].firstChild.data);
+    setWorkspaceName(xml.getElementsByTagName('workspace')[0].firstChild.data);
+    setDocumentName(xml.getElementsByTagName('name')[0].firstChild.data);
+    readBlocks(xml);
+    setTimeout("getUpdates()", 2000);
+}
+
+
+function readBlocks(xml)
+{
+    var blocks = xml.getElementsByTagName('block');
+    for (var i = 0; i < blocks.length; i++)
+    {
+        readBlock(blocks[i]);
+    }
+}
